d59b17f584e477be3ab922bafb3da223152dff16
ihnmalik <ihnmalik@gmail.com>
2018-11-16 08:33:04 +0500

    Commit for heroku

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
+    
     <h1>Hi people</h1>
     <p>Welcome to your Pragma</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
