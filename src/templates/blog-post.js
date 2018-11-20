8159ae6ef4e35ee960bd69dada478ca6c9995840
ihnmalik <ihnmalik@gmail.com>
2018-11-20 18:29:32 +0500

    Language Selection button fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
 
 function BlogPost(props) {
 
@@ -13,7 +12,6 @@ function BlogPost(props) {
             <div className="col-lg-8">
                 <h1 className="mt-4">{title}</h1>
                 <hr />
-                {/* <Img fluid={post.frontmatter.image.childImageSharp.fluid} /> */}
                 <img className="img-fluid rounded" src="http://placehold.it/900x300" alt="" />
                 <hr />
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -55,25 +53,3 @@ export const query = graphql`
        }
    }
 }`
-// export const query = graphql`
-
-//  query PostQuery($slug: String!) {
-//      markdownRemark(fields: { slug: { eq: $slug } }) {
-//        html
-//        frontmatter {
-//         title
-//         description
-//         image {
-//           childImageSharp {
-//             resize(width: 1500, height: 1500) {
-//               src
-//             }
-//             fluid(maxWidth: 786) {
-//               ...GatsbyImageSharpFluid
-//             }
-//           }
-//        }
-//        }
-//    }
-// }
-// `
