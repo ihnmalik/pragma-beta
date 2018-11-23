7306e53891715d26cc2598651b62672b718e1e4e
ihnmalik <ihnmalik@gmail.com>
2018-11-23 12:24:31 +0500

    Navbar font-size fix

--- src/components/HomePageComponents/header/header-main.js
+++ src/components/HomePageComponents/header/header-main.js
@@ -13,9 +13,9 @@ const HeaderMain = (props) => (
       <div className="row align-items-center" style={props.languageName === 'English' ? {textAlign: "justify"} : {textAlign: "right"}}>
         <div className="col-lg-6 mb6 mb-lg-0">
           <div className="pr-md-6 mb-6">
-          <h1 className={`${props.languageName === 'English' ? 'content-headings' : 'content-headings-arabic'}`}>{props.language.main1.title}</h1>
-                 <p className={`${props.languageName === 'English' ? 'headings-details' : 'heading-details-arabic'}`} >{props.language.main1.description}</p>
-                <button className="ApplyBtnHeader">{props.language.main1.apply}</button>
+              <h1 className={`${props.languageName === 'English' ? 'content-headings' : 'content-headings-arabic'}`}>{props.language.main1.title}</h1>
+              <p className={`${props.languageName === 'English' ? 'headings-details' : 'heading-details-arabic'}`} >{props.language.main1.description}</p>
+              <button className="ApplyBtnHeader">{props.language.main1.apply}</button>
           </div>
         </div>
         <div className="col-lg-6 mb6 mb-lg-0">
